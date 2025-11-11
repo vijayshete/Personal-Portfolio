c317c4552777d5a8594c646c1b273cd0a9243ff1
Vijay Shete <vijayshete12345@gmail.com>
2025-11-12 01:50:16 +0530

    Update base path in Vite config and adjust asset links in HTML for deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/Personal-Portfolio/",
   plugins: [react()],
   server: {
     port: 3000,
